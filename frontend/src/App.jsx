7b0c15d2e09e8c52061cd239e308999c2aecff9a
zerrorTwo <namle022004@gmail.com>
2024-12-14 20:36:51 +0700

    add materialUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-
+import Button from '@mui/material/Button';
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Button className="py-10" variant="contained">Hello world</Button>
+
   )
 }
 
