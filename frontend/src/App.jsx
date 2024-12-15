e5dfa963f5f0ce6a122ef3e80e300635a07a432a
zerrorTwo <namle022004@gmail.com>
2024-12-15 09:04:03 +0700

    fix frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
-import Button from '@mui/material/Button';
-function App() {
-  return (
-    <Button className="py-10" variant="contained">Hello world</Button>
+import { Typography } from "@mui/material"
 
+function App() {
+
+  return (
+    <>
+     <Typography> Hihi</Typography>
+    </>
   )
 }
 
