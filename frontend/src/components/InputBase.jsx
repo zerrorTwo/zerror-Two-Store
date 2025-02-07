5a8aaf83e817dd39fba6173910c7b632fbee1d48
zerrorTwo <namle022004@gmail.com>
2025-02-07 15:44:37 +0700

    add data product

--- frontend/src/components/InputBase.jsx
+++ frontend/src/components/InputBase.jsx
@@ -13,7 +13,8 @@ function InputBase({
   required = false,
   multiline = false,
   maxWidth = false,
-  height, // Default height
+  height = "60px",
+  maxHeight,
 }) {
   const theme = useTheme();
 
@@ -33,6 +34,7 @@ function InputBase({
           "& .MuiInputBase-input": {
             color: theme.palette.text.blackColor,
             height: height, // Set custom height
+            maxHeight: maxHeight,
             padding: "8px 12px", // Ensure padding doesn't collapse
             boxSizing: "border-box",
           },
@@ -79,6 +81,7 @@ InputBase.propTypes = {
   multiline: PropTypes.bool,
   maxWidth: PropTypes.bool,
   height: PropTypes.string,
+  maxHeight: PropTypes.string,
 };
 
 export default InputBase;
