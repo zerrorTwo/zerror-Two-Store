1b2f7717b8df3ecc1bc4bfd3f0be22cf7c7efbdc
zerrorTwo <namle022004@gmail.com>
2025-04-12 00:25:05 +0700

    google login

---
+++ frontend/src/constants/gg.constant.js
@@ -0,0 +1,4 @@
+export const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+export const GOOGLE_CLIENT_ID =
+  "849139555901-k1totthh9mjuh19iv0f89b8j2v45vvif.apps.googleusercontent.com"; // Sử dụng biến môi trường React
+export const REDIRECT_URI = `${window.location.origin}/login`;
