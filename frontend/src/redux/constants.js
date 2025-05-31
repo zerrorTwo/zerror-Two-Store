2951d15fd0f88a7b8818fae9622905d743c220e0
zerrorTwo <namle022004@gmail.com>
2025-05-31 19:16:22 +0700

    add be url

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,12 +1,14 @@
-export const PRIMITIVE_URL = "http://localhost:5000";
-export const BASE_URL = "http://localhost:5000/v1/api";
-export const UPLOAD_URL = "http://localhost:5000/v1/api/upload";
-export const USER_URL = "http://localhost:5000/v1/api/users";
-export const PRODUCT_URL = "http://localhost:5000/v1/api/products";
-export const CART_URL = "http://localhost:5000/v1/api/cart";
-export const CHECKOUT_URL = "http://localhost:5000/v1/api/order";
-export const PAYMENT_URL = "http://localhost:5000/v1/api/payment";
-export const ADDRESS_URL = "http://localhost:5000/v1/api/address";
-export const REVIEW_URL = "http://localhost:5000/v1/api/review";
-export const COUPON_URL = "http://localhost:5000/v1/api/coupon";
-export const MAIL_URL = "http://localhost:5000/v1/api/mail";
+export const BE_URL = "https://zerror-two-store.onrender.com";
+
+export const PRIMITIVE_URL = `${BE_URL}`;
+export const BASE_URL = `${BE_URL}/v1/api`;
+export const UPLOAD_URL = `${BE_URL}/v1/api/upload`;
+export const USER_URL = `${BE_URL}/v1/api/users`;
+export const PRODUCT_URL = `${BE_URL}/v1/api/products`;
+export const CART_URL = `${BE_URL}/v1/api/cart`;
+export const CHECKOUT_URL = `${BE_URL}/v1/api/order`;
+export const PAYMENT_URL = `${BE_URL}/v1/api/payment`;
+export const ADDRESS_URL = `${BE_URL}/v1/api/address`;
+export const REVIEW_URL = `${BE_URL}/v1/api/review`;
+export const COUPON_URL = `${BE_URL}/v1/api/coupon`;
+export const MAIL_URL = `${BE_URL}/v1/api/mail`;
