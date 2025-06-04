0c56fad4241f6a364a8381091c003ec850947ff6
zerrorTwo <namle022004@gmail.com>
2025-06-04 22:07:08 +0700

    fix route review

--- backend/src/routes/v1/review.route.js
+++ backend/src/routes/v1/review.route.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { addReview, deleteReview, getAllProductReviews } from "../../controllers/review.controller.js";
+import {
+  addReview,
+  deleteReview,
+  getAllProductReviews,
+} from "../../controllers/review.controller.js";
 import { authentication } from "../../auth/auth.util.js";
 
 const Router = express.Router();
 
+Router.route("/").get(getAllProductReviews);
+
 Router.use(authentication);
-Router.route("/").post(addReview).get(getAllProductReviews);
+Router.route("/").post(addReview);
 Router.route("/delete").delete(deleteReview);
 
 export const reviewRoute = Router;
