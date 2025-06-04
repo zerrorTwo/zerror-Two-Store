6a8dc444028fb4709340f45e8281d97244fd8665
zerrorTwo <namle022004@gmail.com>
2025-06-04 22:01:39 +0700

    fix review route

--- backend/src/constants/white.list.js
+++ backend/src/constants/white.list.js
@@ -1,7 +1,5 @@
 export const WHITELIST_DOMAINS = [
-  "http://localhost:5173",
   process.env.FRONTEND_URL,
   process.env.BACKEND_URL,
-  "localhost:5000",
   "https://open.oapi.vn",
 ];
--- backend/src/routes/v1/review.route.js
+++ backend/src/routes/v1/review.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { addReview, deleteReview, getAllProductReviews } from "../../controllers/review.controller.js";
+import {
+  addReview,
+  deleteReview,
+  getAllProductReviews,
+} from "../../controllers/review.controller.js";
 import { authentication } from "../../auth/auth.util.js";
 
 const Router = express.Router();
-
+Router.use("/").get(getAllProductReviews);
 Router.use(authentication);
-Router.route("/").post(addReview).get(getAllProductReviews);
+Router.route("/").post(addReview);
 Router.route("/delete").delete(deleteReview);
 
 export const reviewRoute = Router;
